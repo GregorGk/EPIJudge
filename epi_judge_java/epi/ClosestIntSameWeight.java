a1c3562cd3c8a5da0a6720c12893603387617267
GregorGk <gorkiewicz.grzegorz@gmail.com>
2019-06-09 12:11:19 +0200

    4.4 Find a closest integer with the same weight

--- epi_judge_java/epi/ClosestIntSameWeight.java
+++ epi_judge_java/epi/ClosestIntSameWeight.java
@@ -1,18 +1,30 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ClosestIntSameWeight {
+
   @EpiTest(testDataFile = "closest_int_same_weight.tsv")
   public static long closestIntSameBitCount(long x) {
-    // TODO - you fill in here.
-    return 0;
+    long lowestSet = x & -x;
+    long lowestUnset = ~x & -~x;
+    if (lowestSet > lowestUnset) {
+      x ^= lowestSet;
+      x ^= lowestSet >>> 1;
+    } else {
+      x ^= lowestUnset;
+      x ^= lowestUnset >>> 1;
+    }
+    return x;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "ClosestIntSameWeight.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {
+                }.getClass().getEnclosingClass())
             .ordinal());
   }
 }
