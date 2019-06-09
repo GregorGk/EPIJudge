653b35a6f3281312ce919fbce8c9c2796584e017
GregorGk <gorkiewicz.grzegorz@gmail.com>
2019-06-09 12:12:56 +0200

    4.5 Compute x×y without arithmetical operators

--- epi_judge_java/epi/PrimitiveMultiply.java
+++ epi_judge_java/epi/PrimitiveMultiply.java
@@ -4,8 +4,25 @@ import epi.test_framework.GenericTest;
 public class PrimitiveMultiply {
   @EpiTest(testDataFile = "primitive_multiply.tsv")
   public static long multiply(long x, long y) {
-    // TODO - you fill in here.
-    return 0;
+    long sum = 0;
+    while(x != 0) {
+      if ((x & 1) != 0) {
+        sum = add(sum, y);
+      }
+      x >>>= 1;
+      y <<= 1;
+    }
+    return sum;
+  }
+
+  public static long add(long a, long b) {
+//   return (b == 0)? a : add(a ^ b, (a & b) << 1);
+    while (b != 0) {
+      long temp = a;
+      a ^= b;
+      b = (temp & b) << 1;
+    }
+    return a;
   }
 
   public static void main(String[] args) {
