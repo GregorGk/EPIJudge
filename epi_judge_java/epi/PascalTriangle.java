d4ffcf1f13521aaaa319b9af426e6f97bb2e1954
GregorGk <gorkiewicz.grzegorz@gmail.com>
2019-07-01 22:53:37 +0200

    5.20 Compute rows in Pascal's Triangle

--- epi_judge_java/epi/PascalTriangle.java
+++ epi_judge_java/epi/PascalTriangle.java
@@ -1,13 +1,26 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.ArrayList;
 import java.util.List;
 public class PascalTriangle {
   @EpiTest(testDataFile = "pascal_triangle.tsv")
 
   public static List<List<Integer>> generatePascalTriangle(int numRows) {
-    // TODO - you fill in here.
-    return null;
+    List<List<Integer>> result = new ArrayList<>();
+    if (numRows > 0) {
+      result.add(List.of(1));
+    }
+    for (int i = 1; i < numRows; ++i) {
+      List<Integer> lastRow = result.get(result.size() - 1);
+      List<Integer> nextRow = new ArrayList<>(List.of(1));
+      for (int j = 0; j < lastRow.size() - 1; ++j) {
+        nextRow.add(lastRow.get(j) + lastRow.get(j + 1));
+      }
+      nextRow.add(1);
+      result.add(nextRow);
+    }
+    return result;
   }
 
   public static void main(String[] args) {
