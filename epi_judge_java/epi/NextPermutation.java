6ede74516d21a8a8a73731e7777fadbb7b83c9fe
GregorGk <gorkiewicz.grzegorz@gmail.com>
2019-07-01 22:47:45 +0200

    5.11 Compute the next permutation

--- epi_judge_java/epi/NextPermutation.java
+++ epi_judge_java/epi/NextPermutation.java
@@ -1,12 +1,25 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.Collections;
 import java.util.List;
 public class NextPermutation {
   @EpiTest(testDataFile = "next_permutation.tsv")
   public static List<Integer> nextPermutation(List<Integer> perm) {
-    // TODO - you fill in here.
-    return null;
+    int inversionIndex = perm.size() - 2;
+    while (inversionIndex >= 0 && perm.get(inversionIndex) >= perm.get(inversionIndex + 1)) {
+      --inversionIndex;
+    }
+    if (inversionIndex < 0)
+      return Collections.emptyList();
+    for (int i = perm.size() - 1; i >= 0; --i) {
+      if (perm.get(i) > perm.get(inversionIndex)) {
+        Collections.swap(perm, inversionIndex, i);
+        break;
+      }
+    }
+    Collections.reverse(perm.subList(inversionIndex + 1, perm.size()));
+    return perm;
   }
 
   public static void main(String[] args) {
