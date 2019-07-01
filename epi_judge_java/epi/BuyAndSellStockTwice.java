70b9e1dabd4c9c1f0544a05162c2126d08d9e88f
GregorGk <gorkiewicz.grzegorz@gmail.com>
2019-07-01 22:42:29 +0200

    5.7 Buy and sell stock twice

--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -1,12 +1,28 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.ArrayList;
 import java.util.List;
 public class BuyAndSellStockTwice {
   @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
   public static double buyAndSellStockTwice(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    double maxTotalProfit = 0.0;
+    List<Double> firstBuySellProfits = new ArrayList<>();
+    double minPriceSoFar = Double.MAX_VALUE;
+
+    for (int i = 0; i < prices.size(); ++i) {
+      minPriceSoFar = Math.min(minPriceSoFar, prices.get(i));
+      maxTotalProfit = Math.max(maxTotalProfit, prices.get(i) - minPriceSoFar);
+      firstBuySellProfits.add(maxTotalProfit);
+    }
+
+    double maxPriceSoFar = 0.0;
+    maxTotalProfit = 0.0;
+    for (int i = prices.size() - 1; i > 0; --i) {
+      maxPriceSoFar = Math.max(maxPriceSoFar, prices.get(i));
+      maxTotalProfit = Math.max(maxTotalProfit, maxPriceSoFar - prices.get(i) + firstBuySellProfits.get(i - 1));
+    }
+    return maxTotalProfit;
   }
 
   public static void main(String[] args) {
