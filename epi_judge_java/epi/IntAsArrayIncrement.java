d6454f81298a6ba0031fb88bbb7942f210bed12b
GregorGk <gorkiewicz.grzegorz@gmail.com>
2019-07-01 22:32:59 +0200

    5.2 Increment an arbitrary-precision integer

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -5,8 +5,18 @@ import java.util.List;
 public class IntAsArrayIncrement {
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
   public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+    int index = A.size() - 1;
+    A.set(index, A.get(index) + 1);
+    while (A.get(index) == 10 && index > 0) {
+      A.set(index, 0);
+      A.set(index - 1, A.get(index - 1) + 1);
+      index--;
+    }
+    if (A.get(index) == 10) {
+      A.set(0, 1);
+      A.add(0);
+    }
+    return A;
   }
 
   public static void main(String[] args) {
